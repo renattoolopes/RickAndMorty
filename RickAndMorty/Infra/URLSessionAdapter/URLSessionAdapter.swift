e307e84dda05484f7716691f868970f78cff01d8
Renato Lopes <renattoolopes@gmail.com>
2020-12-14 20:32:22 -0300

    Created URLSessionAdapter in Infra layer

---
+++ RickAndMorty/Infra/URLSessionAdapter/URLSessionAdapter.swift
@@ -0,0 +1,44 @@
+//
+//  URLSessionAdapter.swift
+//  Infra
+//
+//  Created by Renato Lopes on 14/12/20.
+//
+
+import Foundation
+import Data
+
+public final class URLSessionAdapter: HttpGetClient {
+    // MARK: - Private Properties
+    private let configuration: URLSessionConfiguration
+    
+    // MARK: - Inits
+    public init(configuration: URLSessionConfiguration = .default) {
+        self.configuration = configuration
+    }
+    
+    public func get(url: URL, _ completion: @escaping HttpGetClientResponse) {
+        let session: URLSession = URLSession(configuration: configuration)
+        
+        session.dataTask(with: url) { (data, response, error) in
+            if let httpResponse: HTTPURLResponse = response as? HTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 204:
+                    completion(.success(nil))
+                case 200...299:
+                    completion(.success(data))
+                case 401:
+                    completion(.failure(.unauthorized))
+                case 403:
+                    completion(.failure(.forbidden))
+                case 400...499:
+                    completion(.failure(.badRequest))
+                case 500...599:
+                    completion(.failure(.serverError))
+                default:
+                    completion(.failure(.noConnectivity))
+                }
+            }
+        }
+    }
+}
