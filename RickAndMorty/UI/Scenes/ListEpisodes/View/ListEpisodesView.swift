fc98e67d93aa3bc49702213088225e929814f8cf
Renato Lopes <renattoolopes@gmail.com>
2020-12-14 23:07:15 -0300

    Added setup for style and subviews

--- RickAndMorty/UI/Scenes/ListEpisodes/View/ListEpisodesView.swift
+++ RickAndMorty/UI/Scenes/ListEpisodes/View/ListEpisodesView.swift
@@ -11,12 +11,27 @@ class ListEpisodesView: UIView {
     // MARK: - Private Properties
     private let tableView: UITableView = UITableView(frame: .zero, style: .plain)
     
+    // MARK: - Inits
     init() {
         super.init(frame: .zero)
+        setupSubviews()
+        setupStyle()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    private func setupSubviews() {
+        addSubview(tableView)
+    }
+    
+    private func setupStyle() {
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.separatorInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        tableView.allowsMultipleSelection = false
     }
 }
 
