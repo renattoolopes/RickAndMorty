a5164bc161fead3ea975ed9ab9dc905bc5736a88
Renato Lopes <renattoolopes@gmail.com>
2020-12-14 22:41:15 -0300

    Added function for make view model

--- RickAndMorty/Presentation/Presenter/ListEpisodes/ListEpisodesPresenter.swift
+++ RickAndMorty/Presentation/Presenter/ListEpisodes/ListEpisodesPresenter.swift
@@ -34,14 +34,28 @@ public final class ListEpisodesPresenter {
     
     // MARK: - Public Methods
     public func findAllEpisodes() {
-        listEpisodes.listAll { result in
+        loading.display(LoadingViewModel(isLoading: true))
+        listEpisodes.listAll { [weak self] result in
+            guard let self: ListEpisodesPresenter = self else { return }
             switch result {
             case .success(let episodes):
-                self.reactivity.didCompletedFindAll(episodes: makeEpisodesViewModel(episodes))
-                break
+                self.loading.display(LoadingViewModel(isLoading: true))
+                self.reactivity.didCompletedFindAll(episodes: self.makeEpisodesViewModel(episodes))
             case .failure(let error):
+                self.loading.display(LoadingViewModel(isLoading: true))
                 self.alert.show(AlertViewModel(title: "Ocorreu um erro!", message: error.localizedDescription))
             }
         }
+    }
+    
+    // MARK: - Private Methods
+    private func makeEpisodesViewModel(_ response: [Episode]) -> [EpisodeViewModel] {
+        let viewModels: [EpisodeViewModel] = response.map { episode -> EpisodeViewModel in
+            return EpisodeViewModel(name: episode.name,
+                                    airDate: episode.airDate,
+                                    sessionEpisodeCode: episode.sessionEpisodeCode)
+        }
+        
+        return viewModels
     }
 }
