8e6fad4df6111d4cb49c5531047d1eac61abad40
Renato Lopes <renattoolopes@gmail.com>
2020-12-15 22:16:44 -0300

    Added autolayout in TableView

--- RickAndMorty/UI/Scenes/ListEpisodes/View/ListEpisodesView.swift
+++ RickAndMorty/UI/Scenes/ListEpisodes/View/ListEpisodesView.swift
@@ -15,11 +15,17 @@ class ListEpisodesView: UIView {
     init() {
         super.init(frame: .zero)
         setupSubviews()
+        setupAutoLayout()
         setupStyle()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Public Methods
+    public func registerCellInTableView(_ cell: AnyClass ,withIdentifier: String) {
+        tableView.register(cell.self , forCellReuseIdentifier: withIdentifier)
     }
     
     // MARK: - Private Methods
@@ -32,6 +38,15 @@ class ListEpisodesView: UIView {
         tableView.separatorStyle = .none
         tableView.separatorInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
         tableView.allowsMultipleSelection = false
+    }
+    
+    private func setupAutoLayout() {
+        tableView.translatesAutoresizingMaskIntoConstraints = true
+        
+        tableView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
     }
 }
 
